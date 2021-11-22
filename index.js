d69c23e87f1d048b2c0733e52097a94f5287281f
Simo Elalj <simo@elalj.com>
2021-11-22 17:39:20 +0100

    moved defaultOptions

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const { MongoClient } = require('mongodb');
 
-const defaultConfig = {
-  ...(process.env.MONGO_URL
-    ? { url: process.env.MONGO_URL }
-    : {
-      host: process.env.MONGO_HOST || 'mongo',
-      port: Number(process.env.MONGO_PORT || 27017),
-      username: process.env.MONGO_USERNAME || 'mongo',
-      password: process.env.MONGO_PASSWORD || 'mongo',
-      auth_db: process.env.MONGO_AUTH_DB,
-    }
-  ),
-};
-
-module.exports = (options = defaultConfig) => ({
+module.exports = (customOptions) => ({
   name: 'mongo',
   init: async () => {
+    const defaultConfig = {
+      ...(process.env.MONGO_URL
+        ? { url: process.env.MONGO_URL }
+        : {
+          host: process.env.MONGO_HOST || 'mongo',
+          port: Number(process.env.MONGO_PORT || 27017),
+          username: process.env.MONGO_USERNAME || 'mongo',
+          password: process.env.MONGO_PASSWORD || 'mongo',
+          auth_db: process.env.MONGO_AUTH_DB,
+        }
+      ),
+    };
+
+    const options = customOptions || defaultConfig;
+
     const mongoUrl = (options.url)
       ? options.url
       : `mongodb://${options.username}:${options.password}@${options.host}:${options.port}/${options.auth_db ? options.auth_db : ''}`;
